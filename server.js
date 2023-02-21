46b01d8b319018cf395133dc958889f149940839
George Fleming <seorasproductions@gmail.com>
2023-02-21 12:18:31 +0000

    updated get request to add id and return json with specifc band info

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require("express");
-const app = express();
-const {Musician} = require("./Musician")
-const {sequelize} = require("./db")
+const express = require("express"); // import express
+const app = express(); // calls the express function and stores return value in app variable
+const {Musician} = require("./Musician") // import Musician model
+const {sequelize} = require("./db"); // import sequelize instance
+const { response } = require("express"); // import response
 
-const port = 3000;
+const port = 3000; // set port
 
 //TODO
 
-app.listen(port, () => {
-    sequelize.sync();
-    console.log(`Listening on port ${port}`)
+app.get("/musicians/:id", async (req, res) => { // async function where the /musicians route (endpoint) is defined
+    const musicians = await Musician.findByPk(1); // findByPk() is a sequelize method that returns the musician with the id specified
+    res.json(musicians); // sends the response as json data to the client (browser)
+});
+
+
+
+app.listen(port, () => { // starts the server and listens on the port specified
+    sequelize.sync(); // syncs the database with the models defined in the sequelize instance
+    console.log(`Listening on port ${port}`) 
 })
